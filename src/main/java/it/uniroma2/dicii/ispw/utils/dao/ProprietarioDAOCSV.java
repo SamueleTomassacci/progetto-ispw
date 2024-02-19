f625794c5c9748f90214fcea2afa4be6381d9e3b
flavio-simonelli <flavio.simo2002@gmail.com>
2024-02-19 22:40:02 +0100

    Finito il DAO gestito tramite files

--- src/main/java/it/uniroma2/dicii/ispw/utils/dao/ProprietarioDAOCSV.java
+++ src/main/java/it/uniroma2/dicii/ispw/utils/dao/ProprietarioDAOCSV.java
@@ -10,6 +10,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.nio.file.Files;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -84,6 +85,46 @@ public class ProprietarioDAOCSV {
             exception.initCause(e);
             throw exception;
         }
-
     }
+
+    public void setVip(String username) throws SystemException {
+        try {
+            CSVReader csvReader = new CSVReader(new BufferedReader(new FileReader(fd)));
+            String[] record;
+            StringBuilder updatedCSVContent = new StringBuilder();
+            boolean found = false;
+
+            while ((record = csvReader.readNext()) != null) {
+                int pos = ProprietarioAttributesOrder.getIndex_Username();
+
+                if (record[pos].equals(username)) {
+                    found = true;
+                    String updatedRecord = record[ProprietarioAttributesOrder.getIndex_UserId()] + ","
+                            + record[ProprietarioAttributesOrder.getIndex_Username()] + ","
+                            + record[ProprietarioAttributesOrder.getIndex_Email()] + ","
+                            + record[ProprietarioAttributesOrder.getIndex_Nome()] + ","
+                            + record[ProprietarioAttributesOrder.getIndex_Cognome()] + ","
+                            + "1"; // Imposta il VIP a 1
+
+                    updatedCSVContent.append(updatedRecord).append("\n");
+                } else {
+                    updatedCSVContent.append(String.join(",", record)).append("\n");
+                }
+            }
+
+            if (!found) {
+                SystemException exception = new SystemException("Username non trovato nel CSV");
+                throw exception;
+            }
+
+            // Sovrascrive il file CSV con i dati aggiornati
+            Files.write(fd.toPath(), updatedCSVContent.toString().getBytes());
+
+        } catch (IOException | CsvValidationException e) {
+            SystemException exception = new SystemException();
+            exception.initCause(e);
+            throw exception;
+        }
+    }
+
 }
