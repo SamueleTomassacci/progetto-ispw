bbbaf72c10fab168f56d39c772f19964ba916087
flavio-simonelli <flavio.simo2002@gmail.com>
2024-02-19 22:33:56 +0100

    Aggiunto DAO gestito tramite files

---
+++ src/main/java/it/uniroma2/dicii/ispw/utils/dao/ProprietarioDAOCSV.java
@@ -0,0 +1,89 @@
+package it.uniroma2.dicii.ispw.utils.dao;
+
+import com.opencsv.exceptions.CsvValidationException;
+import it.uniroma2.dicii.ispw.model.GestoreModel;
+import it.uniroma2.dicii.ispw.model.ProprietarioModel;
+import it.uniroma2.dicii.ispw.utils.db.ConnectionDB;
+import it.uniroma2.dicii.ispw.utils.exceptions.SystemException;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import com.opencsv.CSVReader;
+
+public class ProprietarioDAOCSV {
+    private static final String CSV_GESTORE = "csv/proprietario.csv";
+    private File fd;
+
+    private static class ProprietarioAttributesOrder {
+        public static int getIndex_UserId() {
+            return 0;
+        }
+
+        public static int getIndex_Username() {
+            return 1;
+        }
+
+        public static int getIndex_Email() {
+            return 2;
+        }
+
+        public static int getIndex_Nome() {
+            return 3;
+        }
+
+        public static int getIndex_Cognome() {
+            return 4;
+        }
+
+        public static int getIndex_Vip() {
+            return 5;
+        }
+
+    }
+
+    public ProprietarioDAOCSV() throws SystemException {
+        this.fd = new File(CSV_GESTORE);
+        try {
+
+            if (!fd.exists()) {
+                fd.createNewFile();
+            }
+        } catch (IOException e) {
+            SystemException exception = new SystemException();
+            exception.initCause(e);
+            throw exception;
+        }
+
+    }
+
+    public ProprietarioModel getProprietarioByUsername(String username) throws SystemException {
+        ProprietarioModel proprietario = null;
+        try {
+            CSVReader csvReader = new CSVReader(new BufferedReader(new FileReader(fd)));
+            String[] record;
+
+
+            while ((record = csvReader.readNext()) != null) {
+
+                int pos = ProprietarioAttributesOrder.getIndex_Username();
+
+                if (record[pos].equals(username)) {
+                    proprietario = new ProprietarioModel(record[ProprietarioAttributesOrder.getIndex_Username()], record[ProprietarioAttributesOrder.getIndex_Email()], Integer.parseInt(record[ProprietarioAttributesOrder.getIndex_UserId()]), record[ProprietarioAttributesOrder.getIndex_Nome()], record[ProprietarioAttributesOrder.getIndex_Cognome()], Integer.parseInt(record[ProprietarioAttributesOrder.getIndex_Vip()]));
+                }
+            }
+            return proprietario;
+
+        } catch (IOException | CsvValidationException e) {
+            SystemException exception = new SystemException();
+            exception.initCause(e);
+            throw exception;
+        }
+
+    }
+}
